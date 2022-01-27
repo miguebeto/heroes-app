438d395d007f304de4664b8e4eb114f5b40bf864
Miguel Blanco <mindmusic7@gmail.com>
2022-01-27 10:37:03 -0500

    list getHeroByPublisher added

--- src/components/dc/DcScreen.js
+++ src/components/dc/DcScreen.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { HeroList } from '../hero/HeroList';
 
+//muestra el listado de los heroes segun el publisher (Dc o Marvel)
 export const DcScreen = () => {
+
   return (
     <div> 
         <h1>DcScreen</h1> 
+        <hr />
+        
+        <HeroList publisher="DC Comics" />
     </div>
   )
 };
--- src/components/marvel/MarvelScreen.js
+++ src/components/marvel/MarvelScreen.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { HeroList } from '../hero/HeroList';
 
+//muestra el listado de los heroes segun el publisher (Dc o Marvel)
 export const MarvelScreen = () => {
   return (
     <div>
         <h1>MarvelScreen</h1>
+        <hr />
+        
+        <HeroList publisher="Marvel Comics" />
+
     </div>
   )
 };

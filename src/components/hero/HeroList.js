e620bc48d95344fa667b700a5b20cd97136d6ee8
Miguel Blanco <mindmusic7@gmail.com>
2022-01-28 10:01:14 -0500

    animated css added

--- src/components/hero/HeroList.js
+++ src/components/hero/HeroList.js
@@ -4,11 +4,11 @@ import { HeroCard } from './HeroCard';
 
 //este componente se encarga de listar los heroes segun el publisher (Marvel o DC) y pasando sus propiedades al componente HeroCard
 export const HeroList = ({ publisher }) => {
-
+    //usamos el useMemo para que solo se ejecute la funcion cuando cambie el publisher
     const heroes = useMemo(()=> getHeroByPublisher(publisher), [ publisher ]);
 
   return (
-      <div className='row rows-cols-1 row-cols-md-3 g-3'>
+      <div className='row rows-cols-1 row-cols-md-3 g-3 animate__animated animate__fadeIn'>
             {
                 heroes.map( hero => (
                     <HeroCard 

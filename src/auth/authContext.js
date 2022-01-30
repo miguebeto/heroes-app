b65d83bd2abe9232d6ff0de9e81ee4be823302b4
Miguel Blanco <mindmusic7@gmail.com>
2022-01-30 11:15:25 -0500

    Revert "public routes added"
    This reverts commit 93e2bd0599a15eeaf8a36f6527c8f29d6888f3d8.

--- src/auth/authContext.js
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-
-//creamos el contexto
-export const AuthContext = createContext()
--- src/types/types.js
+++
@@ -1,6 +0,0 @@
-
-//creamos los tipos de estados para nuestra autenticacion
-export const types = {
-    login: '[auth] Login',
-    logout: '[auth] Logout',
-}
